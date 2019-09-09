55d97e7e5c50774975df71496534a2786ef7669a
tallua <talluay@gmail.com>
2019-09-10 02:24:41 +0900

    do basic settings and add main class

--- src/main/java/com/tallua/depthshot/DepthShot.java
+++ src/main/java/com/tallua/depthshot/DepthShot.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.tallua.depthshot;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
@@ -7,11 +7,11 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = DepthShot.MODID, name = DepthShot.NAME, version = DepthShot.VERSION)
+public class DepthShot
 {
-    public static final String MODID = "examplemod";
-    public static final String NAME = "Example Mod";
+    public static final String MODID = "depthshot";
+    public static final String NAME = "Depth Shot";
     public static final String VERSION = "1.0";
 
     private static Logger logger;
